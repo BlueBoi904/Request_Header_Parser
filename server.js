c090146f04e1e1eeadffec5048020704fc314184
BlueBoi904 <cliff.mirschel@gmail.com>
2021-07-05 09:18:03 -0700

    Added solution

--- server.js
+++ server.js
@@ -21,8 +21,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const infoObj = {
+    "ipaddress": req.ip,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  }
+  res.json(infoObj);
 });
 
 
